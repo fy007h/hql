a316b0af0240a32e7b57ef24758fce5619524c8d
FFFFFF <oncwnuKDub9hkxgD2rrPwXk6Gzss@git.weixin.qq.com>
2022-08-14 03:21:50 +0800

    Add 天气其他数据

--- main.py
+++ main.py
@@ -5,24 +5,49 @@ from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
-
+from emoji import emojize
 today = datetime.now()
-start_date = os.environ['START_DATE']
-city = os.environ['CITY']
-birthday = os.environ['BIRTHDAY']
-
-app_id = os.environ["APP_ID"]
-app_secret = os.environ["APP_SECRET"]
-
-user_id = os.environ["USER_ID"]
-template_id = os.environ["TEMPLATE_ID"]
-
-
+start_date = "2018-09-07"
+city = "松江区"
+birthday = "11-06"
+app_id = "wx98e8c8cabb4b1d51"
+app_secret = "2b805f6c8c2e3271c16232cade770a4d"
+city_data = "上海"
+user_id = "ozFNU6PNX1sG8WUIwX39XGHX3Ato"
+template_id = "fJN68unFFZ1vXlOy_3cQY4XKSHmBouiByHTjiwpO-To"
+def get_weather_emoji():
+  wea, temperature = get_weather()
+  if wea == "多云":
+    return "\U000026C5"
+  elif wea == "小雨" or wea == "大雨" or wea == "中雨":
+    return "\U0001F327"
+  elif wea == "晴":
+    return "\U0001F31E"
+  elif wea == "阴":
+    return "\U00002601"
+  elif wea == "阵雨":
+    return "\U0001F326)"
+  elif wea == "雷阵雨":
+    return "\U000026C8"
+  else:
+    return "\U0001F31A"
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
+
+def get_weather_other():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['wind'], math.floor(weather['pm25'])
+
+def get_weather_other_1():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  return weather['airQuality'], weather['humidity']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -47,7 +72,10 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
+emoji = get_weather_emoji()
 wea, temperature = get_weather()
-data = {"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
+wind, pm25 = get_weather_other()
+airQuality, humidity = get_weather_other_1()
+data = {"airQuality":{"value":airQuality,"color":get_random_color()},"humidity":{"value":humidity,"color":get_random_color()},"city_data":{"value":city_data,"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"wind":{"value":wind,"color":get_random_color()},"pm25":{"value":pm25,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}, "emoji":{"value":emoji}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
